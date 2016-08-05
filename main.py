8e9650dcc2548dc61c783877f8ea3707881022e6
jessep13 <jessepurice@gmail.com>
2016-08-04 20:13:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,22 +13,33 @@ def setup():
   person = Human(name, age, gender)
   person.test()
 
-
+def credits():
+  print("CREDITS:")
+  print("-------------------------")
+  print("Creator: github user, ravenusmc")
+  print("Contributors: github user, jessep13 (who made the credits and edited the 'main' function). ")
+  print("-------------------------")
 
 def main():
-  print("******************")
-  print("Welcome to Car!!!!")
-  print("******************")
-  print("Would you like to play: ")
-  print("1. Yes")
-  print("2. No")
-  option = int(input("What is your choice? "))
-  while not validMain(option):
-    option = int(input("What is your choice? "))
-  if option == 1:
-    setup()
-  elif option == 2:
-    print("Thank you for trying the program!")
-    print("I hope you come by again!")
+  while True:
+    print("******************")
+    print("Welcome to Car!!!!")
+    print("******************")
+    print("Would you like to play? ")
+    print("1. Yes")
+    print("2. No")
+    print("3. Wait! Who mad this???")
+    option = int(input("What is your choice: "))
+    while not validMain(option):
+      option = int(input("What is your choice: "))
+    if option == 1:
+      setup()
+    elif option == 2:
+      print("Thank you for trying the program!")
+      print("I hope you come by again!")
+      input("PRESS ENTER TO EXIT.")
+      exit()
+    elif option == 3:
+      credits()
 
 main()
